5cbb9dd7f99cd4401a794a81c2ae63273e47d257
Kosy <kosyfrances@yahoo.com>
2015-01-09 19:13:39 +0100

    Edited calculator design

--- calculatorProject.js
+++ calculatorProject.js
@@ -6,33 +6,23 @@ var answer = 0,
 
 function clickNumber(number) {
   var text = document.getElementById("textBox");
-
-  if ((number.value === "") && (text.value === "") && (number.value === ".")) {
-    //This condition enables the decimal point to work after the AC button is pressed
-    text.value = 0.;
-  }  
-  else if (number.value === "CE") {
-    //The CE is like the ON button so it displays a zero in the textbox
+ 
+  if (number.value === "CE") {    
     text.value = 0;
     answer = 0;    
   }
-  else if ((text.value === "0") && (number.value !== ".") || (operatorClicked)) {
-    /* 
-    This condition removes leading zeros
+  /* 
+    The condition below removes leading zeros
     and also allows the decimal point (if clicked)to append to zero instead of overwrting it.
     It also replaces the textbox's value with the next after an operator is clicked. 
-    */
+  */
+  else if ((text.value === "0") && (number.value !== ".") || (operatorClicked)) {    
     text.value = number.value; 
     operatorClicked = false;
   }
-  else if (number.value === "") {
-    //This condition is for AC which acts like the OFF button
-    text.value = "";
-    answer = 0;
-  }
+  //The condition below returns and appends the numbers pressed to the textbox
   else {
-    text.value += number.value; 
-    //This returns and appends the numbers pressed to the textbox
+    text.value += number.value;     
   }
 }
 
@@ -63,7 +53,7 @@ function divide(answer, textInput) {
 function doOperation (operator) {
   var text = document.getElementById("textBox"),
       textInput = parseFloat(text.value);
-
+  
   switch(operator.value) {
     case "+":      
       answer = add(answer, textInput);
@@ -104,7 +94,7 @@ function doOperation (operator) {
       operatorClicked = true;
       break;
 
-    case "=":      
+    case "=":               
       if (lastOperator === "+") {
         answer = add(answer, textInput);
         text.value = answer;
@@ -119,7 +109,12 @@ function doOperation (operator) {
       }
       else if (lastOperator === "/") { 
         answer = divide(answer, textInput);
-        text.value = answer;
+        if (answer === Infinity) {
+          text.value = "cannot divide by zero";
+        }
+        else {
+          text.value = answer; 
+        }
       }
       else {
         text.value = textInput;
@@ -130,5 +125,4 @@ function doOperation (operator) {
   }
   lastOperator = operator.value;
 } 
-
 
